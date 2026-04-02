17690b3f399f574bd17080c127b68d6e03038ac7
arjunvaibhavm <2023cs_arjunvaibhavm_a@nie.ac.in>
2026-04-02 23:24:14 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,27 @@
-import './App.css'
-import ChatWindow from './chatWindow'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Chat from "./pages/Chat";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App(){
+function App() {
   return (
-    <>
-    <div>
-      <ChatWindow currentUserId = "UserA" targetUserId="UserB"/>
-      <ChatWindow currentUserId="UserB" targetUserId="UserA" />
-    </div>
-
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute>
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Login() {
   const [form, setForm] = useState({ username: "", password: "" });
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const res = await axios.post("/api/login", form);
     localStorage.setItem("token", res.data.token);
-    alert("Login successful");
+    navigate("/chat");
   };
 
   return (
