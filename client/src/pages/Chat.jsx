94ab465aeefa798cc6a6b33054c312d9bed9d01f
ChithkalaKSTat <thisisecacc@gmail.com>
2026-04-09 23:46:38 +0530

    updates

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import UsersList from '../UsersList';
+import ChatWindow from '../chatWindow';
+
+export default function Chat() {
+    const token = localStorage.getItem('token');
+    const currentUserId = token
+        ? JSON.parse(atob(token.split('.')[1])).username
+        : null;
+
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    return (
+        <div>
+            <UsersList onSelectUser={(username) => setSelectedUser(username)} />
+            {selectedUser && (
+                <ChatWindow
+                    currentUserId={currentUserId}
+                    targetUserId={selectedUser}
+                />
+            )}
+        </div>
+    );
+}
